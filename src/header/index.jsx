56a4d75ade3dd0234362ff3a4ae74064cafa4aaa
Lucien Denis <westlucien@gmail.com>
2025-08-23 00:33:47 +0200

    First commit du portfolio

---
+++ src/header/index.jsx
@@ -0,0 +1,14 @@
+import "./header.css";
+function Header() {
+  return (
+    <div className="header">
+      <p className="style1">
+        Bienvenue,
+        <br /> je suis DENIS Lucien et vous <br /> êtes sur mon portfolio.
+      </p>
+      <img src="src/assets/pic2.avif" className="techpic" alt=""></img>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/nav/index.jsx
@@ -0,0 +1,13 @@
+import "./nav.css";
+function Nav() {
+  return (
+    <div className="nav">
+      <a href="" class="siteLink">
+        Portfolio <br></br>DENIS Lucien
+      </a>
+      <div class="navButtons"></div>
+    </div>
+  );
+}
+
+export default Nav;
