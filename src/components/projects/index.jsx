5fc1b5ca08b5cc47f3c587c9626cd6cf59529aba
Lucien Denis <westlucien@gmail.com>
2025-09-12 17:35:18 +0200

    Responsive bien avancé

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,24 +1,39 @@
 import "./projects.css";
 import monVieuxGrimoire from "../../assets/monVieuxGrimoire.png";
 import SiteWebForm from "../../assets/SiteWebForm.png";
+import KasaPic from "../../assets/Kasa.png";
 import { useState } from "react";
 import ProjectModale from "./projectmodale";
 
 let projects = [
   {
-    titre: "monVieuxGrimoire",
+    titre: "MonVieuxGrimoire",
     imgsrc: monVieuxGrimoire,
-    description: "Description de mon vieux grimoire",
+    description: `Dans le projet MonVieuxGrimoire l'objectif était de coder la base de données noSQL de ce site pour une chaîne de librairies.
+       Le projet utilise Typescript, Express avec une base de données MongoDB.
+        Le site inclut des opérations CRUD avec middleware d'authentification(JWT), comptes utilisateur(bcrypt),
+        stockage et optimisation d'images(multer et sharp).
+      `,
+    lien: "https://github.com/DenisLucien/MonVieuxGrimoire",
   },
   {
-    titre: "bastarBookingSystem",
+    titre: "BastardBookingSystem",
     imgsrc: SiteWebForm,
-    description: "Description de mon vieux grimoire",
+    description: `Le projet BastardBookingSystem est un projet permettant l'envoi de demandes de représentations pour groupe de musiques.
+      Le but est de pouvoir automatiquement demander des demandes et d'en garder un suivi via crm.
+      Pour se faire on met en place une base de données qui stock des groupes de musiques, lieux/contacts, utilisateurs et échanges.
+      Technologies: Php/Symfony, PostGreSQL/Docker. Autres: mailer (envoi de mails de demandes), API Geoportail(autocomplétion),
+       Stimulus ( bibliotheque js pour page dynamique malgrès le php).
+       Le code est privé car réalisé pour Nicolas Vauché.`,
+    lien: "Aucun",
   },
   {
-    titre: "kasa",
-    imgsrc: monVieuxGrimoire,
-    description: "Description de mon vieux grimoire",
+    titre: "Kasa",
+    imgsrc: KasaPic,
+    description: `Le but du projet Kasa était de refondre entièrement le site Kasa précèdemment en ASP.NET en React.
+    Il fallait également mettre en place des animations et utiliser du SCSS ainsi qu'une architecture de dossier comme
+    définie par l'entreprise.`,
+    lien: "https://github.com/DenisLucien/ProjetKasa",
   },
 ];
 
@@ -48,7 +63,7 @@ function Projects() {
 
       <div className="project" onClick={() => toggleModale(2)}>
         <h3>Projet</h3>
-        <img src={monVieuxGrimoire} alt=""></img>
+        <img src={KasaPic} alt=""></img>
         <p className="projectText"></p>
       </div>
       {modaleproject !== null && (
--- src/components/projects/projectmodale/index.jsx
+++ src/components/projects/projectmodale/index.jsx
@@ -6,12 +6,25 @@ function ProjectModale({ tabProj, numProj, onClose }) {
       console.log("outside");
     }
   }
+  let link, linktext;
+  if (tabProj[numProj].titre === "BastardBookingSystem") {
+    linktext = "Pas de lien disponible";
+    link = "#";
+  } else {
+    linktext = "Lien GIithub du projet";
+    link = tabProj[numProj].lien;
+  }
   return (
     <div className="projectmodale" onClick={handleClickOutside}>
       <h3>{tabProj[numProj].titre}</h3>
       <div className="blocImgDescr">
         <img src={tabProj[numProj].imgsrc} alt=""></img>
-        <p className="modaledescription">{tabProj[numProj].description}</p>
+        <div className="rightside">
+          <p className="modaledescription">{tabProj[numProj].description}</p>
+          <a href={link} target="_blank">
+            {linktext}
+          </a>
+        </div>
       </div>
 
       <button onClick={onClose}>Fermer</button>
